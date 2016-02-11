d0731789c3c8d40d41597e49b86b4d6e8464f76c
rutvikpatel43 <rutvikpatel43@gmail.com>
2016-02-10 23:16:39 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express on Azure' });
+  res.render('index', { title: 'Rutvik Patel' });
 });
+/* Get About Page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+/* Get Cotact Page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+/* Get Interest Page */
+router.get('/interest', function(req, res, next) {
+  res.render('interest', { title: 'My hobbies/ Interest' });
+});
+/* Get project Page */
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Assignment / Project I worked on' });
+});
+
 
 module.exports = router;
